875e627bc3cd00936a9e8a3bcf4d7251a338d259
catsvilles <eugemorrow@gmail.com>
2020-02-02 12:47:19 +0100

    Changed routes. Changed HomeController to allow unregistered users view all posts.

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('main');
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/all/jobposts', 'HomeController@JobPostList')->name('all_jobposts');
 Route::get('/all/articles', 'HomeController@ArticleList')->name('all_articles');
 Route::get('/create/jobpost', 'HomeController@createJobPost')->name('create_job_post');
@@ -27,3 +11,15 @@ Route::get('/edit/job/post/{post_id}', 'HomeController@editJobPost')->name('edit
 Route::get('/edit/article/{post_id}', 'HomeController@editArticle')->name('edit_article_form');
 Route::post('/update/post/{post_id}', 'HomeController@updatePost')->name('update_post');
 Route::post('/delete/post/{post_id}', 'HomeController@deletePost')->name('delete_post');
+
+// Route::domain('{admin}.domain.com')->group(function () {
+//     Auth::routes();
+//     Route::get('/', 'AdminController@index');
+//     Route::get('/drafts', 'AdminController@drafts');
+//     Route::get('/create', 'AdminController@create');
+//     Route::get('/{type}/{slug}', 'AdminController@show');
+//     Route::get('/edit/{type}/{slug}', 'AdminController@edit');
+//     Route::post('/store', 'AdminController@store');
+//     Route::put('/edit/{type}/{slug}', 'AdminController@update');
+//     Route::delete('{type}/{slug}', 'AdminController@destroy');
+// });
