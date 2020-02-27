6b39b7e2bc819be50ccbc81f1007afc1d806e967
StepanovaAnastasiya <stasyod@gmail.com>
2020-02-27 18:01:08 +0200

    created migration to add collumns Slug and Category, added function sluggeble() to Articles model

--- app/Articles.php
+++ app/Articles.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Articles extends Model
 {
+
+  use Sluggable;
+
+  public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
   public function writer()
  {
     return $this->belongsTo('App\User', 'author', 'id');
