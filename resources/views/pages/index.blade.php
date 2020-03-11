c4f6aba80527d7e1a184c2203e45f904bb0a5e0c
StepanovaAnastasiya <stasyod@gmail.com>
2020-03-11 11:57:08 +0200

    changed href in Add button on Index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -68,7 +68,7 @@
 								<h2>About</h2>
 								<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
 								<ul class="actions">
-									<li><a href="{{ url('/home') }}" class="button">Add new article or job post</a></li>
+									<li><a href="{{ url('/profile') }}" class="button">Add new article or job post</a></li>
 								</ul>
 							</section>
 
