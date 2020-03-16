ac54990a5e31ba204575320334fa4497aa691289
StepanovaAnastasiya <stasyod@gmail.com>
2020-03-16 15:52:19 +0200

    chanced edit view

--- resources/views/posts/post_edit.blade.php
+++ resources/views/posts/post_edit.blade.php
@@ -1,32 +1,44 @@
 @extends('layouts.app')
 @section('content')
+<style media="screen">
+  form, button {
+    width: 100%
+  }
+</style>
     <div class="container-fluid">
         <div class="row">
 
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Edit Post</h1>
+                <div class="align-items-center border-bottom">
+                  <h1 class="h1">Edit Post</h1>
                 </div>
                     <form action="{{ route('update_post', ['slug' => $post->slug]) }}" method="post"enctype="multipart/form-data">
                                               {{ csrf_field() }}
-                                              <p><select required name="category">
+                                               <div class="form-group">
+                                               <select required name="category">
                                                <option disabled selected>Please choose category</option>
                                                <option value="1">Job post</option>
                                                <option value="2">Article</option>
-                                              </select></p>
+                                               </select>
+                                               </div>
+                                                </br>
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter article title" name="title" value="{{ $post->title }}" required>
+                            <input type="text" class="form-control" placeholder="Title" name="title" value="{{ $post->title }}" required>
                         </div>
+                         </br>
                         <div class="form-group">
                             <label for="body">Content</label>
                             <textarea class="form-control" rows="5" name="body" required>{{ $post->body }}</textarea>
                         </div>
+                         </br>
                         <div class="form-group">
-                          <label for="exampleFormControlFile1">Feature Image</label>
-                          <input type="file" class="form-control-file" id="exampleFormControlFile1" name="image">
+                          <label>Feature Image</label>
+                          <input type="file" class="form-control-file" name="image">
                       </div>
+                       </br>
+                       <div class="form-group">
                         <button type="submit" class="btn btn-primary">Edit</button>
+                        </div>
                     </form>
             </main>
         </div>
