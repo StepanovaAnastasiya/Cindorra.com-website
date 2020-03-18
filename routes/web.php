456d9fdd48fb5973b7cbbc2821f53eaeebfe7d97
StepanovaAnastasiya <stasyod@gmail.com>
2020-03-18 08:36:57 +0200

    changed route for single post

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 Route::get('/', 'PagesController@index')->name('main');
-Route::get('/explore/{cat_slug?}', 'PostController@explore')->name('explore');
-Route::get('/{cat_slug}/{slug?}', 'PostController@show')->name('single_post');
-
 Auth::routes();
+
+Route::get('/explore/{cat_slug?}', 'PostController@explore')->name('explore');
+Route::get('/post/{cat_slug}/{slug?}', 'PostController@show')->name('single_post');
+
+
 Route::get('/profile', 'UserController@index')->name('home');
 Route::get('/post', 'UserController@createPost')->name('create_post');
 
