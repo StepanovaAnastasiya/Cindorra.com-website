533d7716a5b82df55ab30b8ab78e7fa515dfc063
StepanovaAnastasiya <stasyod@gmail.com>
2020-03-23 18:35:47 +0200

    added share buttons

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,16 +1,24 @@
-<section id="footer">
-  <ul class="icons">
-    <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-    <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-    <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-    <li><a href="#" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
-    <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-  </ul>
-  <p class="copyright">&copy; Untitled. <!-- Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.--></p>
-</section>
 
-<!-- 	<ul class="stats">
-<li><a href="#">General</a></li>
-<li><a href="#" class="icon solid fa-heart">28</a></li>
-<li><a href="#" class="icon solid fa-comment">128</a></li>
-</ul> -->
+<?php
+
+Use Jorenvh\Share\Providers\ShareServiceProvider;
+
+echo Share::page(url()->current(), 'Cindorra.com')
+->facebook()
+->twitter()
+->linkedin('Cindorra')
+->whatsapp();
+?>
+
+<!--  <section id="footer">
+<ul class="icons">
+<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+<li><a href="#" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
+<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+</ul>
+
+</section>
+ -->
+<p class="copyright">&copy; Untitled. <!-- Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.--></p>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
 	<script src="{{ asset('js/breakpoints.min.js') }}" defer></script>
 	<script src="{{ asset('js/util.js') }}" defer></script>
 	<script src="{{ asset('js/main.js') }}" defer></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <script src="{{ asset('js/share.js') }}"></script>
+	<script src="https://kit.fontawesome.com/ba20d21225.js" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" href="{{ asset('css/main.css') }}" />
 </head>
